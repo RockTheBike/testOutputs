cf3be9698189cd84741b93f8822da34cc57d074d
shop <shop@Rock-the-Bike.local>
2016-12-02 18:41:37 -0800

    first commit

---
+++ testOutputs.ino
@@ -0,0 +1,27 @@
+#define BAUD_RATE 57600
+#define NUM_LEDS 12 // Number of LED outputs (includes (halogen) energy sinks)
+const int ledPins[NUM_LEDS] = {  3,4,5,6,7, 8,  9,10,11,12,A5, 13  };
+
+void setup() {
+  Serial.begin(BAUD_RATE);
+
+  Serial.println("test all outputs");
+
+  pinMode(2, OUTPUT);
+
+  // init LED pins
+  for(int i = 0; i < NUM_LEDS; i++) {
+    pinMode(ledPins[i],OUTPUT);
+  }
+}
+
+void loop() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    digitalWrite(ledPins[i],HIGH);
+    Serial.print(i);
+    delay(1000);
+    Serial.print(", ");
+    digitalWrite(ledPins[i],LOW);
+  }
+  Serial.println();
+}
